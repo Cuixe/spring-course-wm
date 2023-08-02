ad6bf55459d2a0d0779f555e71c98f5a652692ae
Hugo Fuentes <hugofuentesmx@gmail.com>
2023-08-02 19:05:21 +0000

    implement actuator on coupon and order to reload the properties dynamically using http://localhost/coupon-services/actuator/refresh

--- day1/order-services/src/main/java/com/walmart/api/order/OrderServiceApplication.java
+++ day1/order-services/src/main/java/com/walmart/api/order/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.walmart.api.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +10,7 @@ import feign.Logger;
 
 @SpringBootApplication
 @EnableFeignClients
+@RefreshScope
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
